08af107bf75475851d2cecd68266c1459c4351a8
ALLAM KARTHIK <163504385+karthik2024a@users.noreply.github.com>
2026-09-22 13:21:58 +0530

    Update AuthController.java

--- auth-project/src/main/java/com/example/authproject/controller/AuthController.java
+++ auth-project/src/main/java/com/example/authproject/controller/AuthController.java
@@ -18,7 +18,7 @@ import java.util.*;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class AuthController {
 
     private final AuthService authService;
