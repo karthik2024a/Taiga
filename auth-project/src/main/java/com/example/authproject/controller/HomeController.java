a168d9aff0824f9fb387f2a5e45dade4bd84dca8
ALLAM KARTHIK <163504385+karthik2024a@users.noreply.github.com>
2026-09-22 13:58:14 +0530

    Create HomeController.java

---
+++ auth-project/src/main/java/com/example/authproject/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.authproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Taiga Backend is Running Successfully 🚀";
+    }
+}
